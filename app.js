edee4150bf5c9f6be4013c10fe59345a32573aa4
Hossein Farahkordmahaleh <h.farah61@gmail.com>
2025-02-04 14:07:10 +0200

    Rebuilt routes and functions

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const allPhones = JSON.parse(
   fs.readFileSync(`${__dirname}/data/phones.json`, "utf-8")
 );
 
-app.get("/api/v1/phones", (req, res) => {
+const getAllPhones = (req, res) => {
   res.status(200).json({
     status: "success",
     count: allPhones.length,
@@ -16,9 +16,9 @@ app.get("/api/v1/phones", (req, res) => {
       phones: allPhones,
     },
   });
-});
+};
 
-app.get("/api/v1/get-phone/:id", (req, res) => {
+const getPhone = (req, res) => {
   const id = req.params.id * 1;
   const phone = allPhones.find((el) => el.id === id);
   if (!phone) {
@@ -33,9 +33,9 @@ app.get("/api/v1/get-phone/:id", (req, res) => {
       phone,
     },
   });
-});
+};
 
-app.post("/api/v1/new-phone", (req, res) => {
+const createPhone = (req, res) => {
   const newId = allPhones[allPhones.length - 1].id + 1;
   const newPhone = Object.assign({ id: newId }, req.body);
   allPhones.push(newPhone);
@@ -51,9 +51,9 @@ app.post("/api/v1/new-phone", (req, res) => {
       });
     }
   );
-});
+};
 
-app.patch("/api/v1/update-phone/:id", (req, res) => {
+const updatePhone = (req, res) => {
   const id = req.params.id * 1;
   const phone = allPhones.find((el) => el.id === id);
   if (!phone) {
@@ -75,30 +75,38 @@ app.patch("/api/v1/update-phone/:id", (req, res) => {
       });
     }
   );
-});
+};
 
-app.delete("/api/v1/delete-phone/:id", (req, res) => {
-    const id = req.params.id * 1;
-    const phone = allPhones.find((el) => el.id === id);
-    if (!phone) {
-        return res.status(404).json({
-        status: "fail",
-        message: "Invalid ID",
-        });
-    }
-    const index = allPhones.indexOf(phone);
-    allPhones.splice(index, 1);
-    fs.writeFile(
-        `${__dirname}/data/phones.json`,
-        JSON.stringify(allPhones),
-        (err) => {
-        res.status(204).json({
-            status: "success",
-            data: null,
-        });
-        }
-    );
+const deletePhone = (req, res) => {
+  const id = req.params.id * 1;
+  const phone = allPhones.find((el) => el.id === id);
+  if (!phone) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
     });
+  }
+  const index = allPhones.indexOf(phone);
+  allPhones.splice(index, 1);
+  fs.writeFile(
+    `${__dirname}/data/phones.json`,
+    JSON.stringify(allPhones),
+    (err) => {
+      res.status(204).json({
+        status: "success",
+        data: null,
+      });
+    }
+  );
+};
+
+app.route("/api/v1/phones").get(getAllPhones).post(createPhone);
+
+app
+  .route("/api/v1/get-phone/:id")
+  .get(getPhone)
+  .patch(updatePhone)
+  .delete(deletePhone);
 
 const port = 3000;
 app.listen(port, () => {
